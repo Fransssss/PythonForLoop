42bf5f89841739db4999e71ce9ecadce29420de1
Frans <97081479+Fransssss@users.noreply.github.com>
2022-06-19 13:02:42 -0700

    Update main.py

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ for i in range(st,fn,step):                                # if step 2 and start
     print(i)
 
 print("\nOutput/display character in word/s one by one")
-word = input("\ninput word/s to be displayed one by one: ")
+word = input("input word/s to be displayed one by one: ")
 for i in word:
     print(i)
 
